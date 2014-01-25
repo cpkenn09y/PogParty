35ba8784ff72dfc22bf4fd72d9d6eb435a39c1b8
Kenneth Yu <kenyu@ucdavis.edu>
2014-01-25 03:28:12 -0800

    able to create instances of pog and append them onto page upon initialize

---
+++ landing.js
@@ -0,0 +1,24 @@
+LOCATIONTOAPPEND = "game-board"
+CSSCLASSOFOBJECT = "pog"
+
+var Pog = function(id, x, y) {
+  this.id = id;
+  this.x = x;
+  this.y = y;
+  this.initialize()
+}
+
+Pog.prototype.initialize = function () {
+  this.appendToBoard()
+}
+
+Pog.prototype.appendToBoard = function() {
+  $('.' + LOCATIONTOAPPEND).append("<div id='" + this.id + "' class='"+ CSSCLASSOFOBJECT + "'>")
+  $('#' + this.id).css({'top': this.y, 'left': this.x})
+}
+
+// $(document).ready(runThisShit)
+
+// function runThisShit() {
+//   var myPog = new Pog(1, 50, 50)
+// }
