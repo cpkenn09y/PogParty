fbe34356df49b443a5800b3b2d8d323ac96b1dd9
Gregory Varias <gregvarias@gmail.com>
2014-01-25 15:54:10 -0800

    added each-turn logic to Game Controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,16 +1,57 @@
 class GameController < ApplicationController
 	
 	def index
-		
 	end
 
-	def drop_slammer(game_id)
+	def run_game
+		game_id = params[:game_id]
+		pog_status = drop_slammer(game_id)
+		return determine_and_set_winner(game_id) if game_over?(game_id)
+		pog_status
+	end
+
+	private
+
+	def game_over?(game_id)
 		game = Game.find(game_id)
+		game.pogs.each do |pog|
+			return false if pog.status == 'unflipped'
+		end
+		true
+	end
+
+	def determine_and_set_winner(game_id)
+		@game = Game.find(game_id)
+		player1_id = @game.player1_id
+		player2_id = @game.player2_id
+
+		player1_winnings = 0
+		player2_winnings = 0
+
+		@game.pogs.each do |pog|
+			pog.user_id == player1_id ? (player1_winnings += 1) : (player2_winnings += 1)
+		end
+
+		if player1_winnings == player2_winnings
+			return 'draw'
+		elsif player1_winnings > player2_winnings
+			player1 = Player.find(player1_id)
+			return player1_id
+		else
+			player2 = Player.find(player2_id)
+			return player2.name
+		end
+	end
+
+	def drop_slammer(game_id, player_id)
+		game = Game.find(game_id)
+		player = Player.find(player_id)
 		flipped 	= []
 		unflipped = []
 		game.pogs.each do |pog|
 			if [true,false].sample == true
 				pog.status = 'flipped'
+				player.pogs << pogs 		# MAKE SURE OVERWRITES FORMER USER_ID
 				flipped << pog
 			else
 				unflipped << pog
