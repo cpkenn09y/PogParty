899760b7db01d33476084e9d9ecdcba20ef0d839
TGOlson <tydotg@gmail.com>
2014-01-25 05:13:14 -0800

    droppable

--- pog.js
+++ pog.js
@@ -1,18 +1,18 @@
-LOCATIONTOAPPEND = "game-board"
-CSSCLASSOFOBJECT = "pog"
+// LOCATIONTOAPPEND = "game-board"
+// CSSCLASSOFOBJECT = "pog"
 
-var Pog = function(id, x, y) {
-  this.id = id;
-  this.x = x;
-  this.y = y;
-  this.initialize()
-}
+// var Pog = function(id, x, y) {
+//   this.id = id;
+//   this.x = x;
+//   this.y = y;
+//   this.initialize()
+// }
 
-Pog.prototype.initialize = function () {
-  this.appendToBoard()
-}
+// Pog.prototype.initialize = function () {
+//   this.appendToBoard()
+// }
 
-Pog.prototype.appendToBoard = function() {
-  $('.' + LOCATIONTOAPPEND).append("<div id='" + this.id + "' class='"+ CSSCLASSOFOBJECT + "'>")
-  $('#' + this.id).css({'top': this.y, 'left': this.x})
-}
+// Pog.prototype.appendToBoard = function() {
+//   $('.' + LOCATIONTOAPPEND).append("<div id='" + this.id + "' class='"+ CSSCLASSOFOBJECT + "'>")
+//   $('#' + this.id).css({'top': this.y, 'left': this.x})
+// }
