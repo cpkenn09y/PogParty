6c7c56837c972121267e3c5e227132a20ff5b9a8
Gregory Varias <gregvarias@gmail.com>
2014-01-25 03:09:07 -0800

    created new rails project pog_party

---
+++ pog_party/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PogParty::Application.config.secret_token = '51f09776907dad5be0b4cc9f6a6617e6d2d99152469905fc53a69ba2bf28a7e7a71477690f3cf760ecba49b4576ab12855a35c058c7b0eab119a525cadae6e6f'
