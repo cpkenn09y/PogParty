01fd54f18b7f07c39e5c241dd0c904e1bf360048
Kenneth Yu <kenyu@ucdavis.edu>
2014-01-25 18:42:49 -0800

    turn the index.js into an ApplicationController constructor

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,49 +1,63 @@
-$(function(){
-	setDragDrop();
-	console.log('shit works');
-})
+var ApplicationController = function() {
 
-function setDragDrop(){
+}
+
+ApplicationController.prototype.setDragDrop = function(){
 	$('.slammer').draggable({ revert: "invalid" });
-
+	self = this
 	$('#pog-stack').droppable({
 		activeClass: "game-board-highlight",
 		hoverClass: "game-board-hover",
 		accept: ".slammer",
-		drop: afterMath
+		drop: self.afterMath
 	})
 }
 
-function afterMath(e, obj){
-	movePogs()
+ApplicationController.prototype.afterMath = function(e, obj){
+	self.movePogs()
 
 	$.ajax({
 	  type: "POST",
 	  url: "/games/run_game/"
 	}).done(function(pogOrientation){
 		faceDownPogs = pogOrientation.face_down
-		visualizeFaceDown(faceDownPogs)
+		self.visualizeFaceDown(faceDownPogs)
+		// Put the number of pogs written on the top one
+		// append a span and center it, for the count
 		// TAKE THEM OUT OF THE DOM
+		// Set TimeOut and then RESET
 	})
 }
 
-function visualizeFaceDown(faceDownPogs) {
+ApplicationController.prototype.visualizeFaceDown = function(faceDownPogs) {
 	faceDownPogs.forEach(function(id) {
 		$('.pog#' + id).addClass('face-down')
 	})
 }
 
-function movePogs(){
+ApplicationController.prototype.movePogs = function(){
 	var pogs = $('.pog')
 	$.each(pogs, function(i, elem){
 		var pogPosition = $(elem).position()
 		$(elem).animate({
-			top:  pogPosition.top + randNum(150),
-			left: pogPosition.left + randNum(300)
+			top:  pogPosition.top + self.randNum(150),
+			left: pogPosition.left + self.randNum(300)
 		})
 	})
 }
 
-function randNum(scale){
+ApplicationController.prototype.randNum = function(scale){
 	return (Math.round(Math.random()) * 2 - 1) * (Math.random() * scale)
 }
+
+ApplicationController.prototype.run = function(){
+	// debugger
+	this.setDragDrop();
+}
+
+
+$(document).ready(function() {
+	// debugger
+	myApplication = new ApplicationController()
+	myApplication.run()
+})
