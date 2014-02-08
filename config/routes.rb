5f6055fed5086fb4d8da7862a2e4ae6ca990c442
garrethdottin <garreth.dottin@gmail.com>
2014-02-07 22:52:00 -0800

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 PogParty::Application.routes.draw do
 
-  # # resources :players, only: [:new, :create] do
-  # 	resources :pogs, only: [:new, :create, :show]
-  # 	resources :games, only: [:new, :create, :show]
-  # end
-  resources :games
-  post "games/:game_id/run_game", to: "games#run_game", as: :run_game
+  # does not consider devise, will revise later
+  resources :players, only: [:new, :create] do
+    resources :pogs, only: [:new, :create, :show]
+    resources :games, only: [:new, :create, :show]
+  end
+
+  # resources :pogs, only: [:new, :create]
+  # resources :games, only: [:index, :create]
+
+  devise_for :players, :controllers => { :omniauth_callbacks => "players/omniauth_callbacks" }
+
+  devise_scope :player do
+     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_player_session
+  end
 
   root :to => 'games#index'
 
